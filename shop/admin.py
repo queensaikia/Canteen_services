d2fb5264817c65ec52f1fc1cc7e5746b1614ee38
queensaikia <queensaikia98@gmail.com>
2019-01-15 22:24:11 -0800

    committed

---
+++ shop/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Category, Product
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'available']
+    list_filter = ['available']
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Product, ProductAdmin)
+
